550bd245e4b5d899aa62317ee486bf4c9adfdaef
Ratnesh Kumar Sharma <rksiitd123@gmail.com>
2025-07-04 15:08:48 +0530

    add link to Join as volunteer button on call to action component

--- components/call-to-action.tsx
+++ components/call-to-action.tsx
@@ -154,8 +154,10 @@ export function CallToAction() {
                 </Link>
               </Button>
               <Button size="lg" variant="outline" className="border-white hover:bg-white/10 text-slate-500">
-                <Users className="w-5 h-5 mr-2" />
-                Join as Volunteer
+                <Link href="/volunteer">
+                  <Users className="w-5 h-5 mr-2" />
+                  Join as Volunteer
+                </Link>
               </Button>
             </div>
           </div>
