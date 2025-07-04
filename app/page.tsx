28d09696c908135757c0ada572ac9fd97b8f9df5
Ratnesh Kumar Sharma <rksiitd123@gmail.com>
2025-07-04 19:00:56 +0530

    remove impact, testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { Hero } from "@/components/hero"
 import { About } from "@/components/about"
-import { Impact } from "@/components/impact"
 import { Programs } from "@/components/programs"
-import { Testimonials } from "@/components/testimonials"
 import { CallToAction } from "@/components/call-to-action"
 
 export default function HomePage() {
@@ -10,9 +8,7 @@ export default function HomePage() {
     <main>
       <Hero />
       <About />
-      <Impact />
       <Programs />
-      <Testimonials />
       <CallToAction />
     </main>
   )
